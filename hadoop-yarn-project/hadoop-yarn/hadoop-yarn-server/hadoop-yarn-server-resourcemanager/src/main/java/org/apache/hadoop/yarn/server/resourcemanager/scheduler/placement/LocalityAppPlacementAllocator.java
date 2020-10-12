ed9820ee50a772aa4af68135d9e4c2f2697f56b0
keywoshilishuang <931400503@qq.com>
2020-10-12 11:57:43 +0800

    4

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/placement/LocalityAppPlacementAllocator.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/placement/LocalityAppPlacementAllocator.java
@@ -131,7 +131,7 @@ public class LocalityAppPlacementAllocator <N extends SchedulerNode>
     Exception stevensli_e = new Exception("stevensli print updatePendingAsk stack:");
     StackTraceElement[] stevensli_trace = stevensli_e.getStackTrace();
     StringBuilder stevensli_sb=new StringBuilder("");
-    stevensli_sb.append(" stevensli LocalityAppPlacementAllocator.java->updatePendingAsk stack");
+    stevensli_sb.append(" stevensli LocalityAppPlacementAllocator.java->updatePendingAsk stack for request:"+requests.toString());
     for (StackTraceElement stackTraceElement : stevensli_trace) {
       stevensli_sb.append("\n\t\tat " + stackTraceElement);
     }
